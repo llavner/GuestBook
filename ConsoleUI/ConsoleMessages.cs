3f848d6ea537476f7013af90e6ed9fc7659eddcb
llavner <marcus.renvall@gmail.com>
2025-07-18 08:47:51 +0200

    Added classes for messages and request data

--- ConsoleUI/ConsoleMessages.cs
+++ ConsoleUI/ConsoleMessages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuestLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,26 @@ namespace ConsoleUI;
 
 public static class ConsoleMessages
 {
+    public static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the GuestBook Practise App");
+        Console.WriteLine("*************************************");
+        Console.WriteLine();
+    }
 
+    public static void GoodByeMessage()
+    {
+        Console.WriteLine("Goodbye and thank you for using the guestbook.");
+    }
+
+    public static void PrintGuestBookMessages(List<GuestBookModel> guestMessages)
+    {
+        foreach (var message in guestMessages)
+        {
+            Console.WriteLine(message.GuestInformation);
+            Console.WriteLine();
+        }
+
+    }
 
 }
--- ConsoleUI/ConsoleReqData.cs
+++ ConsoleUI/ConsoleReqData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuestLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace ConsoleUI;
 
 public static class ConsoleReqData
 {
+    public static void GetGuestInput(List<GuestBookModel> guestMessages)
+    {
+        string isQuiting = string.Empty;
 
+        do
+        {
+            GuestBookModel guest = new();
+
+            Console.Write("First name: ");
+            guest.FirstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            guest.LastName = Console.ReadLine();
+
+            Console.Write("Message: ");
+            guest.GuestBookMessage = Console.ReadLine();
+
+            guestMessages.Add(guest);
+
+            Console.Write("Do you wish to quit? (yes/no)");
+            isQuiting = Console.ReadLine();
+
+        } while (isQuiting != "yes");
+    }
 }
